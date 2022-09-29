c56c38e4af9cbf36cc0776d844976eda89837b51
Vijay-Ky <vijayky007@gmail.com>
2022-09-29 17:51:18 +0530

    first commit

---
+++ Language_Fundamentals/app1/src/M.java
@@ -0,0 +1,16 @@
+class M 
+{
+	public static void main(String[] args) 
+	{
+		/*
+		System.out.println(10 + 10);//20
+		System.out.println(20 - 10);//10
+		System.out.println(10 * 10);//100
+		System.out.println(10 / 2);//5
+		System.out.println(10 % 2);//0
+		*/
+		
+		
+		System.out.println(10 + 2 + ":" + (20 - 2) + ":" + 10 * 2 + ":" + 10 / 2 + ":" + 10 % 2);
+	}
+}
